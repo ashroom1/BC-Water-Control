ad0147d187df5c020431884913931c72c86c8d94
ashroom1 <59590539+ashroom1@users.noreply.github.com>
2020-06-17 03:08:00 -0400

    Update MotorControl.ino

--- MotorControl.ino
+++ MotorControl.ino
@@ -16,28 +16,34 @@
 #define ONs2 "ONs2"
 #define OFF "OFF"
 #define STATUS "STATUS"
-#define TANKDEAD "TANKDEAD"
-#define ON_WITH_TIMER "ON_WITH_TIMER"
+#define ON_WITH_TIMER "ONSolar"
+#define ALL "ALL"
+#define MOTOR "MOTOR"
 
 const char *ssid = "BCWifi";
 const char *password = "Swamy";
 const char *host_name = "hostname_goes_here";
 const char *TOPIC_MotorChange = "MotorStatusChange";
-const char *TOPIC_SensorMalfunction = "SensorMalfunction";
 const char *TOPIC_PingGround = "PingGround";
 const char *TOPIC_SysKill = "SysKill";
 const char *TOPIC_PingTank = "PingTank";
-const char *TOPIC_GroundAwake = "GroundAwake";
-const char *TOPIC_GroundResponse = "GroundResponse";
 const char *TOPIC_TankResponse = "TankResponse";
 const char *TOPIC_ManualOverride = "ManualOverride";
+const char *TOPIC_MotorReset = "MotorReset";
+const char *TOPIC_CurrentMotorState = "CurrentMotorState";
 
 
 bool motor_state;
+bool manualEnable;
+bool manual_state;
+bool tank_responsive;
+int no_response_count = 0;
+
 unsigned long lastTankResponse = 4294967294;
+unsigned long pingTime;
 Ticker ping_tank;
 Ticker tank_response;
-Ticker notify_active_state;
+Ticker BlinkLED;
 Ticker water_timer;
 WiFiClient wclient;
 PubSubClient client(wclient);
@@ -54,32 +60,36 @@ const float timer_s2 = 0;
 
 void setupWiFi() {
 
+  bool manual;
   delay(10);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED)
-    delay(1000);
+  while(WiFi.status() != WL_CONNECTED) {
+    manual = digitalRead(ManualOverride);
+    if(manual)
+      break;
+    delay(300);
+  }  
 }
 
+void tankresponsefun() {
 
-void notifyActive() {
-  client.publish(TOPIC_GroundAwake, ON);
-}
-
-
-void checkTankResponse() {
-
-  unsigned long time_elapsed = abs(lastTankResponse - millis());
-  if (time_elapsed > Seconds(30) && time_elapsed < Minutes(60) * 24 * 40 /* Making sure we don't run into trouble when millis overflows */) {
-    client.publish(TOPIC_PingTank, TANKDEAD);
-    ESP.deepSleep(0);
+  unsigned long elapsed = lastTankResponse - pingTime;
+  if(abs(elapsed) < Minutes(48 * 60 /* 2 days */)) {       //Handle millis overflow
+    if(elapsed <= Seconds(14)) 
+      no_response_count = 0;
+    else {
+      if(no_response_count < 3)
+        ++no_response_count;
+    }
   }
 }
 
 
 void pingNow() {
   client.publish(TOPIC_PingTank, STATUS);
-  tank_response.once(20, checkTankResponse);
+  pingTime = millis();
+  tank_response.once(14, tankresponsefun);
 }
 
 
@@ -87,6 +97,7 @@ void waterTimer() {
 
   if(motor_state) {
     digitalWrite(Motor, LOW);
+    client.publish(TOPIC_CurrentMotorState, OFF);
     motor_state = 0;
   }
 }
@@ -103,37 +114,24 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
     if(!strcmp(message, ON))
       lastTankResponse = millis();
 
-  if(!strcmp(msgTopic, TOPIC_PingGround))
-    if(!strcmp(message, STATUS))
-      client.publish(TOPIC_GroundResponse, ON);
 
   if(!strcmp(msgTopic, TOPIC_SysKill))
-    if(!strcmp(message, OFF)) {
+    if(!strcmp(message, MOTOR) | !strcmp(message, ALL)) {
       if(motor_state)
         {
           digitalWrite(Motor, LOW);
           motor_state = 0;
         }
-      ESP.deepSleep(0);   //TODO: Also turn off motor
-    }
-
-   if(!strcmp(msgTopic, TOPIC_SensorMalfunction))
-    if(!strcmp(message, ON))  {
-      if(motor_state)
-      {
-        digitalWrite(Motor, LOW);
-        motor_state = 0;
-      }
       ESP.deepSleep(0);
     }
 
-
-  if(!strcmp(msgTopic, TOPIC_MotorChange)) {
+  if(!strcmp(msgTopic, TOPIC_MotorChange) && !tank_responsive) {
 
     if(!strcmp(message, OFF)) {
       if(motor_state) {
         digitalWrite(Motor, LOW);
-        motor_state = 0;   
+        motor_state = 0;
+        client.publish(TOPIC_CurrentMotorState, OFF);
       }  
     }
     
@@ -141,6 +139,7 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
       if(!motor_state) {
         motor_state = 1;
         digitalWrite(Motor, HIGH);
+        client.publish(TOPIC_CurrentMotorState, ON);
         water_timer.once(timer_pure_seconds, waterTimer);
       }
     }
@@ -149,6 +148,7 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
       if(!motor_state) {
         motor_state = 1;
         digitalWrite(Motor, HIGH);
+        client.publish(TOPIC_CurrentMotorState, ON);
         water_timer.once(timer_s1s2, waterTimer);
       }
     }
@@ -157,6 +157,7 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
       if(!motor_state) {
         motor_state = 1;
         digitalWrite(Motor, HIGH);
+        client.publish(TOPIC_CurrentMotorState, ON);
         water_timer.once(timer_s1, waterTimer);
       }
     }
@@ -165,68 +166,88 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
       if(!motor_state) {
         motor_state = 1;
         digitalWrite(Motor, HIGH);
+        client.publish(TOPIC_CurrentMotorState, ON);
         water_timer.once(timer_s2, waterTimer);
       }
     }
   }
 }
 
+void blinkfun() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+}
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
   motor_state = 0;
+  tank_responsive = 1;
+  
   pinMode(Motor, OUTPUT);
   pinMode(ManualOverride, INPUT);
   pinMode(ManualControl, INPUT);
-  
-  digitalWrite(Motor, LOW);
+
+  bool manual = digitalRead(ManualOverride);
+
+  if(!manual)
+    digitalWrite(Motor, LOW);
 
   setupWiFi();
 
   client.setServer(host_name, 1883);
   client.setCallback(callback);
 
-  ping_tank.attach(10 * 60, pingNow);
-  notify_active_state.attach(30 * 60, notifyActive);
+  connectMQTT();
+
+  client.publish(TOPIC_CurrentMotorState, OFF);
+  ping_tank.attach(10, pingNow);
+  BlinkLED.attach(10, blinkfun);
 }
 
 
 void connectMQTT() {
 
+  bool manual;
+  
   while (!client.connected()) {
+
+    manual = digitalRead(ManualOverride);
+
+    if(manual)
+      break;
     String clientID = "BCground-";
     clientID += String(random(0xffff), HEX);    //Unique client ID each time
 
     if(client.connect(clientID.c_str())){   //Subscribe to required topics
       client.subscribe(TOPIC_TankResponse);
       client.subscribe(TOPIC_SysKill);
-      client.subscribe(TOPIC_PingGround);
       client.subscribe(TOPIC_MotorChange);
     }
     else
-      delay(Seconds(2.5));  //Try again after a while
+      delay(300);  //Try again after a while
   }
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool manualEnable = digitalRead(ManualOverride);
-  bool manual_state;
+  manualEnable = digitalRead(ManualOverride);
   if(manualEnable) {
     client.publish(TOPIC_ManualOverride, ON);
     manual_state = digitalRead(ManualControl);
-    if(client.connected()) {    //Disconnect MQTT during manual control
-      client.disconnect();
-    }
     
     if(manual_state) {
+      motor_state = 1;
       digitalWrite(Motor, HIGH);
+      client.publish(TOPIC_CurrentMotorState, ON);
     }
     else {
+      motor_state = 0;
       digitalWrite(Motor, LOW);  
+      client.publish(TOPIC_CurrentMotorState, OFF);
     }
   }
   else {
@@ -239,5 +260,5 @@ void loop() {
     client.publish(TOPIC_ManualOverride, OFF);
     client.loop();
   }
-  delay(motor_state ? Seconds(10) : Seconds(30));    //Different delay based on whether the motor is on or off
+  delay(Seconds(1));
 }
