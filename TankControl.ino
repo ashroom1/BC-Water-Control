2ccede4faf974c61fe585650933e85ebc138d3b8
LikithBC <likithbc@gmail.com>
2021-10-17 12:47:52 +0530

    Code Refactoring & alignment

--- TankControl.ino
+++ TankControl.ino
@@ -6,13 +6,13 @@
 #include <Ticker.h>
 #include <EEPROM.h>
 
-#define FIRMWARE_VERSION "0.1.0"
+#define FIRMWARE_VERSION "1.0.0"
 
-#define SECONDS(s) s*1000
-#define MINUTES(m) m*SECONDS(60) //Currently not used
+#define SECONDS(s) (s * 1000)
+#define MINUTES(m) (m * SECONDS(60))    //Currently not used
 #define OFF_MESSAGE_FREQ_MILLISEC SECONDS(5)
 #define MAX_MSG_LENGTH 25
-#define WIFI_INFO_FREQUENCY_SECONDS 10  //Enter values in multiples of 5
+#define WIFI_INFO_FREQUENCY_SECONDS 10    //Enter values in multiples of 5
 
 //  Pin numbers of the sensor
 #define SENSOR1 D5
@@ -21,11 +21,11 @@
 #define EEPROM_INIT_PIN D1
 
 #define ON "ON"
-#define ONs1s3 "ONs1s3"
-#define ONs1 "ONs1"
-#define ONs3 "ONs3"
+#define ONS1S3 "ONS1S3"
+#define ONS1 "ONS1"
+#define ONS3 "ONS3"
 #define OFF "OFF"
-#define ON_WITH_TIMER "ONSolar"
+#define ON_SOLAR_TIMER "ONSOLARTIMER"
 #define STATUS "STATUS"
 #define ALL "ALL"
 #define ACK "ACK"
@@ -44,13 +44,13 @@ const char *TOPIC_BoardResetCountReset = "BoardResetCountReset";
 const char *TOPIC_SysKill = "SysKill";
 const char *TOPIC_PingTank = "PingTank";
 const char *TOPIC_TankResponse = "TankResponse";
-const char *TOPIC_GroundResetAndAcknowledge = "GroundResetAndAcknowledge"; //If Ground RESET- Reset all timers and motor_state () in TOPIC_GroundResetAndAcknowledge
+const char *TOPIC_GroundResetAndAcknowledge = "GroundResetAndAcknowledge";    //If Ground RESET- Reset all timers and motor_state () in TOPIC_GroundResetAndAcknowledge
 const char *TOPIC_SensorMalfunctionReset = "SensorMalfunctionReset";
 const char *TOPIC_WifiInfo = "WifiInfo";
 const char *TOPIC_SystemErrorSensorMalfunction = "SystemError/SensorMalfunction";
 const char *TOPIC_SystemErrorSensorMalfunctionEEPROM = "SystemError/SensorMalfunctionEEPROM";
 
-const int timer_solar_seconds = 7*60; //(6+1=7mins See next line for details) Enter Solar tank Overflow Timer value in seconds
+const int timer_solar_seconds = 7 * 60;   //(6+1 = 7mins See next line for details) Enter Solar tank Overflow Timer value in seconds
 // The value here should be atleast 30s more than actual solar timer otherwise motor might trigger sensor malfunction.
 
 unsigned long lastOffMessage_millis;
@@ -58,7 +58,7 @@ unsigned long lastOffMessage_millis;
 /*
  * --Save sensor malfunction in EEPROM--
  * --Send on message repeatedly--
- * --Don't care-> Send on if on or off if off: Currently sending only OFF msg--
+ * --Don't care-> Send on if on or off if off: Conclusion- Currently sending only OFF msg, ON not required--
  * --Delay to be given for off message frequency.--
  * --Unique LED pattern for WiFi, MQTT etc.--
  * --//const char TOPIC_GroundResetAndAcknowledge = "GroundResetAndAcknowledge"; //To be checked Motor ON/OFF Condition- Toggle, Also check the callback--
@@ -79,14 +79,14 @@ unsigned long lastOffMessage_millis;
  */
 
 /* IMPORTANT NOTE:
-Issue: MotorStatus "OFF"(unintended) message was obtained inbetween "Ons1" messages
+Issue: MotorStatus "OFF"(unintended) message was obtained inbetween "ONS1" messages
 Temporary Fix: Follow steps mentioned here for clearing "SensorMalfunction"
 Permanent Fix: Will be fixed in Ver.02 of software.
 Ref.image: "SensorMalfunction_Persistent-OFFmsg_error"
 
 Status of board when the fix was found
 {
-    1. Current State: 
+    1. Current State:
     D7(S3 Solar  ) - 1
     D6(S2 MainOVF) - 0
     D5(S1 MainMid) - 0
@@ -105,22 +105,22 @@ Steps to clear SensorMalfunction:
     d. RESET the Hardware
 
 Story Number 02:
-    Once upon a time in broker, 
+    Once upon a time in broker,
     the persistant "ON" message (of "SensorMalfunction") previously stored in the broker,
-    was the first thing to be received in the CallBack, 
+    was the first thing to be received in the CallBack,
     which caused it to send one "OFF" message which was a problem.
 */
 
-bool blink_flag;   //Blink Flag interrupt
+bool blink_flag;    //Blink Flag interrupt
 bool motor_state;   //Current state of the motor
-bool solartimer_flag;      //True indicates that the motor is on a pure timer
+bool solartimer_flag;   //True indicates that the motor is on a pure timer
 bool sensor_malfunction;
-bool s1 = 1, s2 = 1, s3 = 1, s1prev = 1, s2prev = 1, s3prev = 1;  //Sensor values
-bool sensorStatusFlag;  //Flag for sending status of all sensors for the first time
+bool s1 = 1, s2 = 1, s3 = 1, s1prev = 1, s2prev = 1, s3prev = 1;    //Sensor values
+bool sensorStatusFlag;    //Flag for sending status of all sensors for the first time
 bool onTimerFlag;
 bool EEPROM_Write_Flag;
 bool EEPROM_Value_To_Write;
-unsigned short WifiInfo_flag;    //Non boolean flag (Considered true when value >= WIFI_INFO_FREQUENCY_SECONDS ÷ 5)
+unsigned short WifiInfo_flag;   //Non boolean flag (Considered true when value >= WIFI_INFO_FREQUENCY_SECONDS ÷ 5)
 
 WiFiClient wclient;
 PubSubClient client(wclient);
@@ -129,19 +129,21 @@ AsyncWebServer server(80);
 Ticker timer_to_reset;
 Ticker timer_5sec;
 
-void timer_fun_5sec() {
+void timer_fun_5sec()
+{
     blink_flag = 1;
     ++WifiInfo_flag;
 }
 
-uint8_t EEPROM_read_with_delay(int location_read) {
+uint8_t EEPROM_read_with_delay(int location_read)
+{
     uint8_t temp = EEPROM.read(location_read);
     delay(10);
     return temp;
 }
 
-bool EEPROM_write(int location, int value_to_be_written) {
-
+bool EEPROM_write(int location, int value_to_be_written)
+{
     if(EEPROM_read_with_delay(location) == value_to_be_written)
         return true;
     else {
@@ -159,22 +161,23 @@ bool EEPROM_write(int location, int value_to_be_written) {
     return false;
 }
 
-void resetVar() {
+void resetVar()
+{
     solartimer_flag = 1;
 }
 
-void increaseResetCount() {
-
+void increaseResetCount()
+{
     int temp_increaseResetCount01 = EEPROM_read_with_delay(1);
     int temp_increaseResetCount02 = EEPROM_read_with_delay(2);
     int temp_increaseResetCount03 = EEPROM_read_with_delay(3);
 
-    if (temp_increaseResetCount01 == 0xff && temp_increaseResetCount02 == 0xff) {
+    if(temp_increaseResetCount01 == 0xff && temp_increaseResetCount02 == 0xff) {
         EEPROM_write(1, 0);
         EEPROM_write(2, 0);
         EEPROM_write(3, temp_increaseResetCount03 + 1);
     }
-    else if (temp_increaseResetCount01 == 0xff) {
+    else if(temp_increaseResetCount01 == 0xff) {
         EEPROM_write(1, 0);
         EEPROM_write(2, temp_increaseResetCount02 + 1);
     }
@@ -182,16 +185,16 @@ void increaseResetCount() {
         EEPROM_write(1, temp_increaseResetCount01 + 1);
 }
 
-bool check_and_publish(const char *Topic, const char *Message, bool Persistance) {/*Return type added because ternary op was expecting to return value*/
-
+bool check_and_publish(const char *Topic, const char *Message, bool Persistance)    //Return type added because ternary op was expecting to return value
+{
     if((WiFi.status() == WL_CONNECTED) && (client.connected()))
         Persistance ? client.publish(Topic, (uint8_t*)Message, strlen(Message), true) : client.publish(Topic, Message);
-    return 0;//See block comment above -bool, '0' is a dummy Value.
+    return 0;   //See block comment above -bool, '0' is a dummy Value.
 }
 
-void setupWiFi() {
-
-    digitalWrite(LED_BUILTIN, LOW);     //LED always ON in this function
+void setupWiFi()
+{
+    digitalWrite(LED_BUILTIN, LOW);   //LED always ON in this function
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
@@ -203,19 +206,20 @@ void setupWiFi() {
     delay(10);
 }
 
-void connectMQTT() {
-// Commented- as it is added inside while loop below,         
+void connectMQTT()
+{
+// Commented- as it is added inside while loop below,
 //     if(WiFi.status() != WL_CONNECTED)
 //         setupWiFi();
 
-    while (!client.connected()){
+    while(!client.connected()) {
         if(WiFi.status() != WL_CONNECTED)
-            setupWiFi(); // Connect to Wi-Fi if it gets disconnected in b/w
+            setupWiFi();    // Connect to Wi-Fi if it gets disconnected in b/w
 
         String clientID = "BCterrace-";
         clientID += String(random(0xffff), HEX);    //Unique client ID each time
 
-        if(client.connect(clientID.c_str())){   //Subscribe to required topics
+        if(client.connect(clientID.c_str())) {    //Subscribe to required topics
             client.subscribe(TOPIC_GroundResetAndAcknowledge);
             client.subscribe(TOPIC_SysKill);
             client.subscribe(TOPIC_PingTank);
@@ -237,20 +241,19 @@ void connectMQTT() {
     }
 }
 
-void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
-
+void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength)
+{
     static char message[MAX_MSG_LENGTH + 1];
 
     memcpy(message, (char *) msgPayload, msgLength);
     message[msgLength] = '\0';
 
-    if(!sensor_malfunction) {
+    if(!sensor_malfunction)
         if(!strcmp(msgTopic, TOPIC_PingTank))
             if(!strcmp(message, STATUS))
                 check_and_publish(TOPIC_TankResponse, ON, 0);
-    }
 
-    if(!strcmp(msgTopic, TOPIC_SensorMalfunction)) {
+    if(!strcmp(msgTopic, TOPIC_SensorMalfunction))
         if(!strcmp(message, ON)) {
             EEPROM_Value_To_Write = 1;
             EEPROM_Write_Flag = !EEPROM_write(0, EEPROM_Value_To_Write);
@@ -261,7 +264,6 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
 
             sensor_malfunction = 1;
         }
-    }
 
     if(!strcmp(msgTopic, TOPIC_SensorMalfunctionReset))
         if(!strcmp(message, ON)) {
@@ -293,20 +295,19 @@ void callback(char *msgTopic, byte *msgPayload, unsigned int msgLength) {
  * Turn OFF motor when Ground RESET is received.
  ****Story end****/
     if(!strcmp(msgTopic, TOPIC_GroundResetAndAcknowledge))
-        if(!strcmp(message, ON))
-        {
-            motor_state = 0; //Check comment below
+        if(!strcmp(message, ON)) {
+            motor_state = 0;    //Check comment below
             solartimer_flag = 0;
             onTimerFlag = 0;
-//            check_and_publish(TOPIC_MotorStatusChange, OFF, 0); // Commented due to Toggle issue NEVER UNCOMMENT THIS- (This will be taken care in setup() of MOTOR when it gets RESET)
+//            check_and_publish(TOPIC_MotorStatusChange, OFF, 0); //Commented due to Toggle issue NEVER UNCOMMENT THIS- (This will be taken care in setup() of MOTOR when it gets RESET)
             lastOffMessage_millis = millis();
-            timer_to_reset.detach();      //If motor was ON due to solar timer, we need to stop the ticker because the motor is now OFF.
+            timer_to_reset.detach();    //If motor was ON due to solar timer, we need to stop the ticker because the motor is now OFF.
             check_and_publish(TOPIC_GroundResetAndAcknowledge, ACK, 1);
         }
 }
 
-void setup() {
-
+void setup()
+{
     blink_flag = 0;
     motor_state = 0;
     solartimer_flag = 0;
@@ -322,16 +323,16 @@ void setup() {
     pinMode(SENSOR3, INPUT);
     pinMode(EEPROM_INIT_PIN, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);// Initial state Of LED Active Low->specifying explicitly
+    digitalWrite(LED_BUILTIN, LOW);   //Initial state Of LED Active Low->specifying explicitly
 
     WiFi.hostname("NodeMCU Tank");
     WiFi.setOutputPower(20.5);
-    WiFi.setAutoReconnect(true); //WiFi auto reconnect enabled - No need to call setupWifi() repeatedly but it is for safety 
+    WiFi.setAutoReconnect(true);    //WiFi auto reconnect enabled - No need to call setupWifi() repeatedly but it is for safety
     setupWiFi();
-    
-    AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+
+    AsyncElegantOTA.begin(&server);    //Start ElegantOTA
     server.begin();
-    
+
     EEPROM.begin(10);
 
     increaseResetCount();
@@ -346,7 +347,7 @@ void setup() {
         check_and_publish(TOPIC_SensorMalfunction, OFF, 1); //This will handle EEPROM fail case(write)
         check_and_publish(TOPIC_SensorMalfunctionReset, ON, 0); //This will handle EEPROM fail case(write)
     }
-    else  { //This will handle EEPROM fail case(write)
+    else  {   //This will handle EEPROM fail case(write)
         if(EEPROM_read_with_delay(0)) {
             check_and_publish(TOPIC_SensorMalfunction, ON, 1);
             check_and_publish(TOPIC_SystemErrorSensorMalfunctionEEPROM, "Sensor Malfunction 3", 1);
@@ -354,11 +355,11 @@ void setup() {
         }
         else {
             check_and_publish(TOPIC_SensorMalfunction, OFF, 1);
-            check_and_publish(TOPIC_SensorMalfunctionReset, ON, 0);//added due to Motor Program dependency -Backup message to be sent from RPi as well
+            check_and_publish(TOPIC_SensorMalfunctionReset, ON, 0);   //added due to Motor Program dependency -Backup message to be sent from RPi as well
         }
-    } //This will handle EEPROM fail case(write)
+    }   //This will handle EEPROM fail case(write)
 
-    for(int i=0; i<=10; i++){
+    for(int i=0; i<=10; i++) {
         digitalWrite(LED_BUILTIN, LOW);
         delay(500);
         digitalWrite(LED_BUILTIN, HIGH);
@@ -369,10 +370,11 @@ void setup() {
 
 }
 
-void loop() {
+void loop()
+{
     // put your main code here, to run repeatedly:
-    if(blink_flag){
-        digitalWrite(LED_BUILTIN, LOW);  //Active low
+    if(blink_flag) {
+        digitalWrite(LED_BUILTIN, LOW);   //Active low
         delay(500);
         digitalWrite(LED_BUILTIN, HIGH);
         blink_flag = 0;
@@ -396,22 +398,22 @@ void loop() {
     }
 
     static char Local_WifiData[300];
-    if (WifiInfo_flag >= WIFI_INFO_FREQUENCY_SECONDS / 5) {
-        uint8_t macAddr[6];
-        char *thislocalIP = (char *) &WiFi.localIP().v4();
-        uint8_t *bssid = WiFi.BSSID();
-        WiFi.macAddress(macAddr);
+    if(WifiInfo_flag >= WIFI_INFO_FREQUENCY_SECONDS / 5) {
+        uint8_t Local_macAddr[6];
+        char *Local_thisIP = (char *) &WiFi.localIP().v4();
+        uint8_t *Local_bssid = WiFi.BSSID();
+        WiFi.macAddress(Local_macAddr);
 
-        uint32_t resetCount = 0;
+        uint32_t Local_resetCount = 0;
 
-        // Merging 3 bytes EEPROM data into 1 unsigned int
-        resetCount |= (uint32_t) EEPROM_read_with_delay(3);
-        resetCount *= 256; // Left shift 8 bits
-        resetCount |= (uint32_t) EEPROM_read_with_delay(2);
-        resetCount *= 256; // Left shift 8 bits
-        resetCount |= (uint32_t) EEPROM_read_with_delay(1);
+        //Merging 3 bytes EEPROM data into 1 unsigned int
+        Local_resetCount |= (uint32_t) EEPROM_read_with_delay(3);
+        Local_resetCount *= 256;    //Left shift 8 bits
+        Local_resetCount |= (uint32_t) EEPROM_read_with_delay(2);
+        Local_resetCount *= 256;    //Left shift 8 bits
+        Local_resetCount |= (uint32_t) EEPROM_read_with_delay(1);
 
-        sprintf(Local_WifiData, "Tank\nFirmware Version: %s\nRSSI: %d dBm\nBoard reset count: %u\nIP: %d.%d.%d.%d\nFree heap size: %d\nRouter MAC: %02x:%02x:%02x:%02x:%02x:%02x\nESP MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", FIRMWARE_VERSION, WiFi.RSSI(), resetCount, *thislocalIP, *(thislocalIP + 1), *(thislocalIP + 2), *(thislocalIP + 3), ESP.getFreeHeap(), bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5], macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+        sprintf(Local_WifiData, "Tank\nFirmware Version: %s\nRSSI: %d dBm\nBoard reset count: %u\nIP: %d.%d.%d.%d\nFree heap size: %d\nRouter MAC: %02x:%02x:%02x:%02x:%02x:%02x\nESP MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", FIRMWARE_VERSION, WiFi.RSSI(), Local_resetCount, *Local_thisIP, *(Local_thisIP + 1), *(Local_thisIP + 2), *(Local_thisIP + 3), ESP.getFreeHeap(), Local_bssid[0], Local_bssid[1], Local_bssid[2], Local_bssid[3], Local_bssid[4], Local_bssid[5], Local_macAddr[0], Local_macAddr[1], Local_macAddr[2], Local_macAddr[3], Local_macAddr[4], Local_macAddr[5]);
         check_and_publish(TOPIC_WifiInfo, Local_WifiData, 0);
         WifiInfo_flag = 0;
     }
@@ -433,11 +435,11 @@ void loop() {
             //if(!motor_state) {    //Comment if ON message is to be sent multiple times
 
             if(!s1 && !s3)
-                check_and_publish(TOPIC_MotorStatusChange, ONs1s3, 0);
+                check_and_publish(TOPIC_MotorStatusChange, ONS1S3, 0);
             else if(!s1)
-                check_and_publish(TOPIC_MotorStatusChange, ONs1, 0);
+                check_and_publish(TOPIC_MotorStatusChange, ONS1, 0);
             else if(!s3)
-                check_and_publish(TOPIC_MotorStatusChange, ONs3, 0);
+                check_and_publish(TOPIC_MotorStatusChange, ONS3, 0);
 
             motor_state = 1;
             //}
@@ -457,32 +459,30 @@ void loop() {
             sensor_malfunction = 1;
         }
 
-        else if (s2 && !s3 && s1) {
+        else if(s2 && !s3 && s1) {
             //Use timer to turn on
-            check_and_publish(TOPIC_MotorStatusChange, ON_WITH_TIMER, 0);
+            check_and_publish(TOPIC_MotorStatusChange, ON_SOLAR_TIMER, 0);
             onTimerFlag = 1;
-            // if(!motor_state) { 
+            // if(!motor_state) {
             motor_state = 1;
             timer_to_reset.detach();
             timer_to_reset.once(timer_solar_seconds, resetVar);
-            // } 
+            // }
         }
 
-        else if (s1 && !s2 && s3) {
+        else if(s1 && !s2 && s3)
             //Don't care
-            if ((!motor_state) && (millis() - lastOffMessage_millis >= OFF_MESSAGE_FREQ_MILLISEC)){
+            if((!motor_state) && (millis() - lastOffMessage_millis >= OFF_MESSAGE_FREQ_MILLISEC)) {
                 check_and_publish(TOPIC_MotorStatusChange, OFF, 0);
                 lastOffMessage_millis = millis();
             }
-        }
 
-        else if(!onTimerFlag){  //if solar timer is On don't send off message
-            if(!motor_state){
-                if(millis() - lastOffMessage_millis >= OFF_MESSAGE_FREQ_MILLISEC){
+        else if(!onTimerFlag) {   //if solar timer is On don't send off message
+            if(!motor_state)
+                if(millis() - lastOffMessage_millis >= OFF_MESSAGE_FREQ_MILLISEC) {
                     check_and_publish(TOPIC_MotorStatusChange, OFF, 0);
                     lastOffMessage_millis = millis();
                 }
-            }
             else {
                 check_and_publish(TOPIC_MotorStatusChange, OFF, 0);
                 motor_state = 0;
@@ -491,13 +491,12 @@ void loop() {
         }
 
         //Report changes to sensor values
-        if (sensorStatusFlag){   //
+        if(sensorStatusFlag) {
             check_and_publish(TOPIC_MainTankMid, (s1 ? ON : OFF), 1);
             check_and_publish(TOPIC_MainTankOVF, (s2 ? ON : OFF), 1);
             check_and_publish(TOPIC_SolarTankMid, (s3 ? ON : OFF), 1);
             sensorStatusFlag = 0;
         }
-
         else {
             s1 ^ s1prev ? check_and_publish(TOPIC_MainTankMid, (s1 ? ON : OFF), 1) : 0;
             s2 ^ s2prev ? check_and_publish(TOPIC_MainTankOVF, (s2 ? ON : OFF), 1) : 0;
